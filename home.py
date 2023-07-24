b568c48b06c5a247a4bc8bcb9ac10f8da2b661eb
Ricardo Fernandes <67656688+Ricardojnf33@users.noreply.github.com>
2023-07-24 17:24:32 -0300

    Update home.py

--- home.py
+++ home.py
@@ -6,29 +6,29 @@ from dotenv import load_dotenv
 load_dotenv()
 
 st.set_page_config(
-    page_title="Home",
+    page_title="Faz tudo com OpenAI app",
     page_icon="🐆",
     layout='wide'
 )
 
 # Função para carregar o código do chatbot
 def load_chatbot_code():
-    with open("Pag2_SideBar_Chatbot.py", "r") as file:
+    with open("2_Chatbot.py", "r") as file:
         return file.read()
 
 # Função para carregar o código do chat com DataFrames
 def load_chat_with_df_code():
-    with open("pag4_LangChainChatPandas.py", "r") as file:
+    with open("4_LangChain_ChatPandas.py", "r") as file:
         return file.read()
 
 # Função para carregar o código do chat com PDFs
 def load_chat_with_pdf_code():
-    with open("Pag3_openAIchatPDFv2CGPT_main_.py", "r") as file:
+    with open("3_Chat_PDF.py", "r") as file:
         return file.read()
 
 # Função para carregar o código do chat com busca
 def load_chat_with_search_code():
-    with open("pag5_Chat_with_search.py", "r") as file:
+    with open("5_Chat_with_search.py", "r") as file:
         return file.read()
 
 st.sidebar.markdown("# Faz Tudo app")
